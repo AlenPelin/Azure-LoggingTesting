2ddd18ea984bef9ec21ff9ffc41588ed3fcc59d0
Alen Pelin <alenpelin@gmail.com>
2018-02-13 14:34:02 +1100

    Add prototype

---
+++ LoggingTesting/Controllers/HomeController.cs
@@ -0,0 +1,129 @@
+﻿using System.Web.Mvc;
+
+namespace LoggingTesting.Controllers
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Diagnostics;
+  using System.IO;
+  using System.Linq;
+  using System.Threading;
+  using System.Web.Hosting;
+
+  using log4net;
+
+  public class HomeController : Controller
+  {
+    private static readonly ILog Log = LogManager.GetLogger(typeof(HomeController));
+    private static readonly object SyncRoot = new object();
+    private static readonly List<Thread> Threads = new List<Thread>();
+
+    public ActionResult Index()
+    {
+      lock (SyncRoot)
+      {
+        return Content($"Active threads: {Threads.Count}, log size: {new DirectoryInfo(MvcApplication.LogDir).GetFiles().Sum(x => x.Length) / 1024 / 1024 }MB<br />{Result}");
+      }
+    }
+    
+    // Beware that 10M log records is 1300MB of log file; Azure S1-S3 give only 100GB of space
+
+    public ActionResult StartWithSpin(int id = 10000000)
+    {
+      return StartJob(id, spin: 100);      
+    }
+
+    public ActionResult Start(int id = 10000000)
+    {
+      return StartJob(id, spin: 0);
+    }
+
+    private ActionResult StartJob(int id, int spin)
+    {
+      Thread thread = null;
+      thread = new Thread(() =>
+      {
+        var reps = id;
+        var start = Process.GetCurrentProcess().PrivateMemorySize64;
+
+        var sw = new Stopwatch();
+        var sw2 = new Stopwatch();
+
+        Log.Info("This is example log entry that is intended to test performance");
+
+        sw2.Start();
+
+        if (spin > 0)
+        {
+          for (var i = 0; i < reps; ++i)
+          {
+            Thread.SpinWait(spin);
+
+            sw.Start();
+            Log.Info("This is example log entry that is intended to test performance");
+            sw.Stop();
+          }
+        }
+        else
+        {
+          for (var i = 0; i < reps; ++i)
+          {
+            sw.Start();
+            Log.Info("This is example log entry that is intended to test performance");
+            sw.Stop();
+          }
+        }
+
+        sw2.Stop();
+
+        var end = Process.GetCurrentProcess().PrivateMemorySize64;
+
+        var startMb = start / 1024 / 1024;
+        var endMb = end / 1024 / 1024;
+
+        var msPerK = (double)sw.ElapsedMilliseconds * 1000 / reps;
+
+        lock (SyncRoot)
+        {
+          Result += $"<hr />" +
+                    $"{DateTime.UtcNow:s} INFO " +
+                    $"{msPerK}ms per 1000 in batch of {reps:N1} " +
+                    $"when writing directly to {MvcApplication.LogPath}. " +
+                    $"Total time spent {sw2.Elapsed} including {spin}-spin waits before every log. " +
+                    $"Memory at start: {startMb}MB, at end {endMb}MB";
+
+          // ReSharper disable once AccessToModifiedClosure
+          Threads.Remove(thread);
+        }
+      });
+
+      lock (SyncRoot)
+      {
+        Threads.Add(thread);
+
+        thread.Start();
+      }
+
+      return Content($"Started job for {id:N1} writes with {spin}-spin waits before every log");
+    }
+
+    public ActionResult Cleanup()
+    {
+      Directory.Delete(MvcApplication.LogDir, true);
+
+      return Content("DONE");
+    }
+
+    public static string Result
+    {
+      get
+      {
+        return System.IO.File.ReadAllText(HostingEnvironment.MapPath("/App_Data/result.txt"));
+      }
+      set
+      {
+        System.IO.File.WriteAllText(HostingEnvironment.MapPath("/App_Data/result.txt"), value);
+      }
+    }
+  }
+}
---
+++ LoggingTesting/Global.asax.cs
@@ -0,0 +1,72 @@
+﻿namespace LoggingTesting
+{
+  using System;
+  using System.IO;
+  using System.Text;
+  using System.Web.Hosting;
+  using System.Web.Mvc;
+  using System.Web.Routing;
+
+  using log4net;
+  using log4net.Appender;
+  using log4net.Layout;
+  using log4net.Repository.Hierarchy;
+  using log4net.spi;
+
+  using LoggingTesting.Controllers;
+
+  public class MvcApplication : System.Web.HttpApplication
+  {
+    public static readonly string LogDir = HostingEnvironment.MapPath($"/App_Data/{Environment.MachineName}");
+    public static readonly string LogPath = Path.Combine(LogDir, $"log.{DateTime.Now:yy-MM-dd.HH-mm-ss}.txt");
+
+    protected void Application_Start()
+    {
+      AreaRegistration.RegisterAllAreas();
+
+      RouteTable.Routes.MapRoute(
+        name: "Default",
+        url: "{controller}/{action}/{id}",
+        defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+
+      InitLogger();
+    }
+
+    public static readonly StringBuilder InMemLog = new StringBuilder();
+
+    private static void InitLogger()
+    {
+      Directory.Delete(HostingEnvironment.MapPath($"/App_Data"), true);
+      Directory.CreateDirectory(LogDir);
+
+      File.WriteAllText(LogPath, "");
+
+      var pattern = new PatternLayout { ConversionPattern = "%d [%2%t] %-5p [%-10c]   %m%n%n" };
+      pattern.ActivateOptions();
+
+      var hierarchy = (Hierarchy)LogManager.GetLoggerRepository();
+      hierarchy.Root.RemoveAllAppenders(); /*Remove any other appenders*/
+
+      var mbPerFile = 100; // 10MB is optimal for Sitecore, 100MB for stress test because it takes couple seconds to produce 10MB of logs
+      var fileAppender = new RollingFileAppender
+      {
+        AppendToFile = true,
+        File = LogPath,
+        Layout = pattern,
+        MaxFileSize = mbPerFile * 1024 * 1024,
+        RollingStyle = RollingFileAppender.RollingMode.Size,
+        MaxSizeRollBackups = -1,        
+        ImmediateFlush = false,
+        Threshold = Level.ALL,
+      };
+
+      fileAppender.ActivateOptions();
+
+      log4net.Config.BasicConfigurator.Configure(fileAppender);
+
+      LogManager.GetLogger(typeof(MvcApplication)).Info("Init");
+
+      HomeController.Result = "";
+    }
+  }
+}
